fb862024b1045433f71d5922313358b097ab020f
Aya El Mataoui <ayelmataoui@gmail.com>
2025-06-02 19:26:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-if (confirm("Hey ! Tu voudrait bien une blague en attendant notre magazine ? 😄")) {
-    fetch('https://v2.jokeapi.dev/joke/Any?lang=fr') // lang=fr = pour français (mais limité)
-    .then(response => response.json())
-    .then(data => {
-    // S'il y a une seule ligne de blague (type "single")
-    if (data.type === "single") {
-        alert(data.joke);
-    } 
-    // S'il y a une question + réponse (type "twopart")
-    else if (data.type === "twopart") {
-        alert(data.setup + "\n...\n" + data.delivery);
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    if (confirm("Hey ! Tu voudrais bien une blague en attendant notre magazine ? 😄")) {
+      fetch('https://v2.jokeapi.dev/joke/Any?lang=fr')
+        .then(response => response.json())
+        .then(data => {
+          if (data.type === "single") {
+            alert(data.joke);
+          } else if (data.type === "twopart") {
+            alert(data.setup + "\n...\n" + data.delivery);
+          }
+        })
+        .catch(error => {
+          alert("Oops, blague introuvable 😢");
+          console.error(error);
+        });
+    } else {
+      alert("Pas de blague aujourd'hui... peut-être demain ! 😅");
     }
-    })
-    .catch(error => {
-    alert("Oops, blague introuvable 😢");
-    console.error(error);
-    });
-  } else {
-    alert("Pas de blague aujourd'hui... peut-être demain ! 😅");
-}
+  }, 2000); // 2 secondes d’attente
+});
